43cb0a09a41d3f846a57fced04215e08c1dfd16b
Loïc ENT <loic@admin.fr>
2014-06-12 10:20:23 +0200

    >>> 2.0 <<< _mise en prod responsive

--- index.php
+++ index.php
@@ -257,7 +257,7 @@
             <div class="row">
 
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 footer">
-                    <p class="footer-text">v1.3.1<!-- <marquee scrollamount="5"  DIRECTION="up"> --><!-- </marquee> --></p>
+                    <p class="footer-text">v2.0<!-- <marquee scrollamount="5"  DIRECTION="up"> --><!-- </marquee> --></p>
                 </div><!-- /.footer --> 
             </div>
 
