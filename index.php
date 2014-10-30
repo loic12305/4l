354aacf2a620d2369758d56e3a6f0b9c78ecd3d2
Loïc <loic@minac>
2014-10-30 12:05:46 +0100

    >>- 2.3.0 -<< > Captcha en prod

--- index.php
+++ index.php
@@ -261,7 +261,7 @@
             <div class="row">
 
               <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 footer">
-                <p class="footer-text">v2.2.0<!-- <marquee scrollamount="5"  DIRECTION="up"> --><!-- </marquee> --></p>
+                <p class="footer-text">v2.3.0<!-- <marquee scrollamount="5"  DIRECTION="up"> --><!-- </marquee> --></p>
               </div><!-- /.footer --> 
             </div>
 
